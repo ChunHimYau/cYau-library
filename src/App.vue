4003ffaa24b40cd1db81433c79d2e024f8d8e06e
cyau0010 <cyau0010@student.monash.edu>
2025-08-07 20:08:49 +1000

    Upload Vue JSON lab project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import JSONLab from './components/JSON.vue'
+</script>
+
+<template>
+  <JSONLab />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+/* You can remove unused styles if you want */
+</style>
